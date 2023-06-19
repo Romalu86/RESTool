febfd916ff177118eb3d84a0e154c61f5a84beda
Romalu86 <romalu96@gmail.com>
2023-06-19 21:37:32 +0300

    ClearPreviousResult fix

--- RESTool/Functions/file_functions.h
+++ RESTool/Functions/file_functions.h
@@ -147,37 +147,6 @@
 		return str;
 	}
 
-	// Delete files and folder unpacked_inis
-	void RemoveAllFilesInDirectory(const std::wstring& directory)
-	{
-		std::wstring searchPath = directory + L"\\*.*";
-		WIN32_FIND_DATAW findData;
-		HANDLE hFind = FindFirstFileW(searchPath.c_str(), &findData);
-
-		if (hFind != INVALID_HANDLE_VALUE)
-		{
-			do
-			{
-				if (findData.cFileName[0] != L'.')
-				{
-					std::wstring filePath = directory + L"\\" + findData.cFileName;
-
-					if (findData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)
-					{
-						RemoveAllFilesInDirectory(filePath);
-						RemoveDirectoryW(filePath.c_str());
-					}
-					else
-					{
-						DeleteFileW(filePath.c_str());
-					}
-				}
-			} while (FindNextFileW(hFind, &findData) != 0);
-
-			FindClose(hFind);
-		}
-	}
-
 	// Clear Previous Results confirmation func
 	void ClearPreviousResults()
 	{
@@ -210,7 +179,25 @@
 							if (findData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)
 							{
 								std::wstring subFolderPath = folderPath + L"\\" + findData.cFileName;
-								RemoveAllFilesInDirectory(subFolderPath);
+
+								std::wstring subSearchPath = subFolderPath + L"\\*.*";
+								WIN32_FIND_DATAW subFindData;
+								HANDLE hSubFind = FindFirstFileW(subSearchPath.c_str(), &subFindData);
+
+								if (hSubFind != INVALID_HANDLE_VALUE)
+								{
+									do
+									{
+										if (subFindData.cFileName[0] != L'.')
+										{
+											std::wstring subFilePath = subFolderPath + L"\\" + subFindData.cFileName;
+											DeleteFileW(subFilePath.c_str());
+										}
+									} while (FindNextFileW(hSubFind, &subFindData) != 0);
+
+									FindClose(hSubFind);
+								}
+
 								RemoveDirectoryW(subFolderPath.c_str());
 							}
 							else
