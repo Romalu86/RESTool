4a17f91fbc4034a7f18a3823450c5501c7f70447
Romalu86 <romalu96@gmail.com>
2023-06-30 20:47:51 +0500

    !Release content folder

---
+++ !Release folder/!MakeDB_OE_new.bat
@@ -0,0 +1,5 @@
+del test.db
+bin\ini2db unpacked_inis\CNST.ini test.db
+bin\ini2db unpacked_inis\WEAP.ini test.db
+bin\ini2db unpacked_inis\OBJ.ini test.db
+bin\ini2db unpacked_inis\SFX.ini test.db
---
+++ !Release folder/!MakeRES_Locoland.bat
@@ -0,0 +1,5 @@
+del test.res
+bin\ini2res unpacked_inis\OBJ.ini test.res
+bin\ini2res unpacked_inis\CNST.ini test.res
+bin\ini2res unpacked_inis\WEAP.ini test.res
+bin\ini2res unpacked_inis\SFX.ini test.res
